6045ea1a0f277ed444c9d6ffb18a10a21e75707e
Iain Emsley <iain.emsley@kellogg.ox.ac.uk>
2015-09-21 09:38:00 +0100

    Updated the tests with assertions

--- spec/features/steps/readfile.py
+++ spec/features/steps/readfile.py
@@ -5,17 +5,32 @@ from behave import *
 import os.path
 
 fh = None
-@given('we have a file of integers called "integers.txt"')
-def step_impl(context):
-    if os.path.isfile("../data/integers.txt"):
+@given('we have a file called "{fname}"')
+def step_impl(context,fname):
+    if os.path.isfile("../data/" + fname):
         assert True
-    
 
-@when('we open the file')
-def step_impl(context):
-    fh = open("../data/integers.txt")
+@given('we have a non-existent file called "{fname}"')
+def step_impl(context,fname):
+    if not os.path.isfile("../data/" + fname):
+        assert True
+
+@when('we open the file "{fname}"')
+def step_impl(context, fname):
+    fh = open("../data/" + fname)
     assert fh
 
 @then('we have a filehandle object')
 def step_impl(context):
     pass
+
+@when('we try to open the file "{fname}"')
+def step_impl(context, fname):
+    try:
+        fh = open("../data/" + fname)
+    except Exception,e:
+        context.exc = e
+
+@then('we have an error')
+def step_impl(context):
+    context.exc.message
