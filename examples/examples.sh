253899e48cc1a8055c8542328592cef8a29abd29
Iain Emsley <iain.emsley@kellogg.ox.ac.uk>
2016-01-04 10:38:02 +0000

    Typos fixed and else clause added

--- examples/examples.sh
+++ examples/examples.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-$TYPE=$1
+TYPE=$1
 
-if [ ${TYPE} == "entropy" ]; then
-   echo "Running the plain entropy ChucK example"
+if [ "$TYPE" == "entropy" ] 
+then
    chuck ../src/Log.ck ../src/File.ck ../src/Play.ck  fent.ck:data.txt
-else [ ${TYPE} == "entropyrules" ]; then
+elif [ "$TYPE" == "entropyrules" ] 
+then
    chuck ../src/Rules.ck EntRules.ck ../src/Log.ck ../src/File.ck ../src/Play.ck  fentrules.ck:data.txt
+else
+   echo "$1 not supported. "
 fi
