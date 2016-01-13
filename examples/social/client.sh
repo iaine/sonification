18d4a0b015832ad219bfa914b44ed746138ed3c4
Iain Emsley <iain.emsley@kellogg.ox.ac.uk>
2016-01-13 14:07:18 +0000

    Fix to imports

--- examples/social/client.sh
+++ examples/social/client.sh
@@ -8,4 +8,4 @@ FILE=""
 SRC="../../src/"
 
 
-chuck ${SRC}Play.ck ${SRC}Log.ck ${SRC}Socket.ck ${SRC}Rules.ck ClientRules.ck socialplay.ck:${HOST}:${PORT}:${CHANNEL}:${TYPE}
+chuck ${SRC}File.ck ${SRC}Config.ck ${SRC}Play.ck ${SRC}Log.ck ${SRC}Socket.ck ${SRC}Rules.ck ClientRules.ck socialplay.ck:${HOST}:${PORT}:${CHANNEL}:${TYPE}
