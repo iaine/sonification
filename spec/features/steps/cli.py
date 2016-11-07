a7b9033360ca7d050156187bf7f5c1bc41200d5d
Iain <iain.emsley@kellogg.ox.ac.uk>
2016-11-07 22:03:43 +0000

    updated the text context

--- spec/features/steps/cli.py
+++ spec/features/steps/cli.py
@@ -13,11 +13,11 @@ def step_impl(context):
 @when('I run "{chuck}" with "{fname}"')
 def step_impl(context, chuck, fname):
     #raise NotImplementedError(u'STEP: When I use the "filename"')
-    resp = subprocess.call(['chuck', '../mock/' + chuck + ':' + fname])
+    context.resp = subprocess.call(['chuck', '../mock/' + chuck + ':' + fname])
 
 @then('I get "{response}"')
 def step_impl(context, response):
     '''
         Use Subprocess to run Chuck file on CLI
     '''
-    assert resp    
+    assert context.resp    
